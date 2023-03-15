7e19b17a08cd466e1d4b08a2f1870c4d35d695f1
Spike <sternbergjan6@gmail.com>
2023-03-15 20:31:48 +0100

    Add files via upload

--- starttestapp.bat
+++ starttestapp.bat
@@ -1,4 +1,13 @@
 @echo off
+
+python --version >nul 2>&1
+if %errorlevel% NEQ 0 (
+    echo Python is not installed on this system.
+    python
+) else (
+    echo Python is installed on this system.
+)
+
 python3 -m pip install PySide6
 python -m pip install requests
 python3 testappstarter.py
