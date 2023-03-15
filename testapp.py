66a63120b850700659acb9953c7ddfde98cfb09f
Spike <sternbergjan6@gmail.com>
2023-03-15 04:20:14 +0100

    Add files via upload

--- testapp.py
+++ testapp.py
@@ -1,1 +1,15 @@
-print("hello world")
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Mein PySide6-Fenster")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
