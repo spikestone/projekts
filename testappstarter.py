088b12c2bc38cb5e72f87bde1de471b420ac158e
Spike <sternbergjan6@gmail.com>
2023-03-15 04:02:39 +0100

    Add files via upload

--- testappstarter.py
+++ testappstarter.py
@@ -1,39 +1,27 @@
-import subprocess
+import os
 import requests
-import hashlib
-import os
-import shutil
 
-result = subprocess.run(['pip', 'install', "PySide6"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# URL des Skripts auf GitHub
+github_url = "https://raw.githubusercontent.com/spikestone/projekts/main/testapp.py"
+# Pfad zur lokalen Datei des Skripts
+current_directory = os.getcwd()
+local_path = current_directory + "/testapp.py"
 
-if result.returncode == 0:
-    print('Das Paket wurde erfolgreich installiert.')
+# Überprüfen, ob die lokale Datei existiert
+if os.path.isfile(local_path):
+    # Herunterladen des Skripts von GitHub
+    response = requests.get(github_url)
+    # Vergleichen der beiden Skripte
+    if response.text != open(local_path, 'r').read():
+        # Wenn die Skripte unterschiedlich sind, die neue Datei herunterladen
+        with open(local_path, 'w') as file:
+            file.write(response.text)
+        print("Das Skript wurde aktualisiert!")
+    else:
+        print("Das Skript ist bereits auf dem neuesten Stand!")
 else:
-    print('Fehler beim Installieren des Pakets:', result.stderr.decode())
-
-
-import subprocess
-import os
-
-# Setze das Git-Repository und den Pfad zum Skript
-repo_url = 'https://github.com/spikestone/projekts'
-script_path = 'projekts/testapp.py'
-
-# Wechsle in das Verzeichnis, in dem sich das Skript befindet
-os.chdir(os.path.dirname(script_path))
-
-# Hole die neueste Version des Repositorys
-subprocess.run(['git', 'fetch'])
-
-# Überprüfe, ob es eine neue Version des Skripts gibt
-result = subprocess.run(['git', 'rev-list', 'HEAD...origin/master', '--count'], stdout=subprocess.PIPE)
-num_commits_behind = int(result.stdout.decode('utf-8').strip())
-if num_commits_behind == 0:
-    print('Das Skript ist auf dem neuesten Stand.')
-    exit()
-
-# Lade die neueste Version des Skripts herunter
-subprocess.run(['git', 'pull'])
-
-# Gib eine Meldung aus, dass das Skript aktualisiert wurde
-print('Das Skript wurde aktualisiert.')
+    # Wenn die lokale Datei nicht existiert, die Datei herunterladen
+    response = requests.get(github_url)
+    with open(local_path, 'w') as file:
+        file.write(response.text)
+    print("Das Skript wurde heruntergeladen!")
