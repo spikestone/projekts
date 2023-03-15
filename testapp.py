3c64ac3b0a3f9b5c7d40541aaa62be5da8e2cea2
Spike <sternbergjan6@gmail.com>
2023-03-15 18:23:51 +0100

    Add files via upload

--- testapp.py
+++ testapp.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
@@ -10,23 +11,22 @@ class MainWindow(QWidget):
         #labels
         self.textlabel1 = QLabel("Costum Programme")
         #Knöpfe zum drücken
-        Button1 = QPushButton("Taschenrechner")
-        Button1.clicked.connect
+        self.Button1 = QPushButton("Taschenrechner")
+        self.Button1.clicked.connect(self.on_button_click)
         self.Button2 = QPushButton("Leer")
 
         #grid aka wie die ganzen widgets angeordnet sind
         layout = QGridLayout()
         self.setLayout(layout)
         layout.addWidget(self.textlabel1,0,0)
-        layout.addWidget(Button1,1,0)
+        layout.addWidget(self.Button1,1,0)
         layout.addWidget(self.Button2,2,0)
 
 
 
-        #action bei knopfdruck
-    def on_Eingabe1_click(self):
-        print("helloo")
-        pass
+    #action bei knopfdruck
+    def on_button_click(self):
+        subprocess.call(["python3", "CustomTaschenrechner.py"])
 
 if __name__ == __name__:
     app = QApplication(sys.argv)
