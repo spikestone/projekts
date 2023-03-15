603aa17f2485ad30b45583c46d895a56b0a6a7f7
Spike <sternbergjan6@gmail.com>
2023-03-15 18:41:08 +0100

    Add files via upload

--- CustomTaschenrechner.py
+++ CustomTaschenrechner.py
@@ -5,18 +5,26 @@ from PySide6.QtGui import *
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        #fesnter größe
-        self.resize(100,100)
+
         #label
-        self.label1 = QLabel("Taschenrechner")
+        self.label1 = QLabel()
+        self.label1.setStyleSheet("border: 2px solid black;")
+        self.label2 = QLabel("helo")
+        self.label2.setStyleSheet("border: 2px solid black;")
         #eingabe
         self.Eingabe1 = QLineEdit()
+        self.Eingabe2 = QLineEdit()
+
 
         # layout
         layout = QGridLayout()
         self.setLayout(layout)
+        layout.addWidget(self.label1,1,2)
         layout.addWidget(self.Eingabe1,1,1)
-        layout.addWidget(self.label1,0,0,1,1)
+        layout.addWidget(self.Eingabe2,1,3)
+        layout.addWidget(self.label2,2,1,1,3)
+
+
 
 if __name__ == __name__:
     app = QApplication(sys.argv)
