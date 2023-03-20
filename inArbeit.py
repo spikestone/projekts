0d667fd642ed9ace42ebb1c20da0f5af2caeb42b
Spike <sternbergjan6@gmail.com>
2023-03-20 07:51:56 +0100

    Add files via upload

--- inArbeit.py
+++ inArbeit.py
@@ -1,21 +1,109 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QLabel, QPushButton
-
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
 class App1(QWidget):
     def __init__(self):
         super().__init__()
 
         # Create UI elements
-        label = QLabel("This is App 1")
-        button = QPushButton("Click me")
 
-        # Set up layout
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        layout.addWidget(button)
+        # styles
+        schlank = "border: 1px solid black;"
+        breit = "border: 2px solid black;"
+        # label
+        self.label1 = QLabel()
+        self.label1.setStyleSheet(schlank)
+        self.label2 = QLabel()
+        self.label2.setStyleSheet("border: 2px solid black;")
+        # eingabe
+        self.Eingabe1 = QLineEdit()
+        self.Eingabe1.setStyleSheet(schlank)
+        self.Eingabe1.setValidator(QDoubleValidator())
+        self.Eingabe2 = QLineEdit()
+        self.Eingabe2.setStyleSheet(schlank)
+        self.Eingabe2.setValidator(QDoubleValidator())
+        # Knoepfe
+        self.Plusbutton = QPushButton("Plus +")
+        self.Plusbutton.clicked.connect(self.on_Plus_clicked)
+        self.Minusbutton = QPushButton("Minus -")
+        self.Minusbutton.clicked.connect(self.on_Minus_clicked)
+        self.Malbutton = QPushButton("Mal *")
+        self.Malbutton.clicked.connect(self.on_Mal_clicked)
+        self.Geteiltbutton = QPushButton("Geteilt /")
+        self.Geteiltbutton.clicked.connect(self.on_Geteilt_clicked)
 
-        # Set layout for the widget
+        # layout
+        layout = QGridLayout()
         self.setLayout(layout)
+        layout.addWidget(self.label1, 1, 2)
+        layout.addWidget(self.Eingabe1, 1, 1)
+        layout.addWidget(self.Eingabe2, 1, 3)
+        layout.addWidget(self.label2, 2, 1, 1, 3)
+        layout.addWidget(self.Plusbutton, 3, 1)
+        layout.addWidget(self.Minusbutton, 3, 2)
+        layout.addWidget(self.Malbutton, 3, 3)
+        layout.addWidget(self.Geteiltbutton, 4, 1)
+
+    def on_Plus_clicked(self):
+        self.label1.setText("+")
+        input1_text = self.Eingabe1.text()
+        try:
+            input_number1 = int(input1_text)
+        except ValueError:
+            input_number1 = float(input1_text)
+        input2_text = self.Eingabe2.text()
+        try:
+            input_number2 = int(input2_text)
+        except ValueError:
+            input_number2 = float(input2_text)
+        result = input_number1 + input_number2
+        self.label2.setText(f'Das Ergebnis ist: {result}')
+
+    def on_Minus_clicked(self):
+        self.label1.setText("-")
+        input1_text = self.Eingabe1.text()
+        try:
+            input_number1 = int(input1_text)
+        except ValueError:
+            input_number1 = float(input1_text)
+        input2_text = self.Eingabe2.text()
+        try:
+            input_number2 = int(input2_text)
+        except ValueError:
+            input_number2 = float(input2_text)
+        result = input_number1 - input_number2
+        self.label2.setText(f'Das Ergebnis ist: {result}')
+
+    def on_Mal_clicked(self):
+        self.label1.setText("-")
+        input1_text = self.Eingabe1.text()
+        try:
+            input_number1 = int(input1_text)
+        except ValueError:
+            input_number1 = float(input1_text)
+        input2_text = self.Eingabe2.text()
+        try:
+            input_number2 = int(input2_text)
+        except ValueError:
+            input_number2 = float(input2_text)
+        result = input_number1 * input_number2
+        self.label2.setText(f'Das Ergebnis ist: {result}')
+
+    def on_Geteilt_clicked(self):
+        self.label1.setText("-")
+        input1_text = self.Eingabe1.text()
+        try:
+            input_number1 = int(input1_text)
+        except ValueError:
+            input_number1 = float(input1_text)
+        input2_text = self.Eingabe2.text()
+        try:
+            input_number2 = int(input2_text)
+        except ValueError:
+            input_number2 = float(input2_text)
+        result = input_number1 / input_number2
+        self.label2.setText(f'Das Ergebnis ist: {result}')
 
 class App2(QWidget):
     def __init__(self):
